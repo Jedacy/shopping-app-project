00544ca8b47c244117fb8f85c3f1f4a837bf9bd8
Jedacy <80461666+Jedacy@users.noreply.github.com>
2021-11-18 18:30:14 +0000

    Add files via upload
    Updated Version with some errors

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import {statusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './screens/Login';
+import Home from './screens/Home';
 
 export default function App() {
+  const Stack = createStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View styles={{flex: 1, backgroundColor: 'orange'}}>
+     
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+  }
+})
